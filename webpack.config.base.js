1819551fd08617ce65a19127bd4b251ce01d0d92
yaolh <347046045@qq.com>
2020-08-11 23:39:25 +0800

    dfas

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -3,26 +3,31 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   module: {
     rules: [{
-      test: /\.scss$/,
-      use: [{
+        test: /\.less$/,
+        loader: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [{
           loader: "style-loader" // 将 JS 字符串生成为 style 节点
-      }, {
+        }, {
           loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
-      }, {
-          loader: "sass-loader" ,
-          options:{
+        }, {
+          loader: "sass-loader",
+          options: {
             implementation: require('dart-sass')
-          }// 将 Sass 编译成 CSS
-      }]
-    }]
+          }, // 将 Sass 编译成 CSS
+
+        }],
+
+      },
+    ]
   },
   entry: './src/index.js', //路径名
   plugins: [new HtmlWebpackPlugin({
-      title: 'webpack',
-      template: 'src/index.html'
-    }),
-  ],
-
+    title: 'webpack',
+    template: 'src/index.html'
+  }), ],
   output: {
     filename: 'index.[contenthash].js'
   },
